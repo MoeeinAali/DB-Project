8505695b94c6d132158f530ef5c07f136deec9a8
Moeein Aali <moeeeinaali@gmail.com>
2024-06-29 11:14:15 +0330

    Update SQL Query7

--- Postgres/Q7.sql
+++ Postgres/Q7.sql
@@ -8,6 +8,8 @@ FROM trip_reservation
          JOIN trip ON trip_reservation.trip_number = trip.trip_id
          JOIN vehicle ON trip.vehicle_name = vehicle.vehicle_name
 WHERE trip_reservation.state != 'CANCELLED'
+  AND '1403-01-01' <= trip.start_date
+  AND trip.start_date <= '1403-01-13'
 GROUP BY CASE
              WHEN vehicle.type IN ('bus train', 'koope train') THEN 'train'
              ELSE vehicle.type
