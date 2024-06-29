e69f4f2c8ca82ac0b8aa50e9d767c09fc16b861e
Moeein Aali <moeeeinaali@gmail.com>
2024-06-29 21:12:12 +0330

    Fix All APIs

--- Rest_API/main.js
+++ Rest_API/main.js
@@ -5,6 +5,8 @@ fastify.register(require("@fastify/postgres"), {
 
 });
 
+// Example:
+// http://localhost:3000/balance?userId=1
 fastify.get("/balance", async (req, res) => {
 	const userId = req.query?.userId;
 
@@ -26,7 +28,6 @@ fastify.get("/balance", async (req, res) => {
 			return res.status(404).send("User not found");
 		}
 
-		console.log(result.rows[0]);
 		res.send(result.rows[0]);
 	} catch (err) {
 		console.error("Database query error: ", err);
@@ -34,59 +35,127 @@ fastify.get("/balance", async (req, res) => {
 	}
 });
 
-
+// Example:
+// localhost:3000/charge
+//	{
+//		"userId": 1,
+//		"amount": 1000
+//	}
 fastify.post("/charge", async (req, res) => {
 	const userId = req.body?.userId;
 	const amount = req.body?.amount;
+
+	const query = `
+	SELECT ACCOUNT_NUMBER
+ 	FROM bank_user
+ 	WHERE user_id = $1;
+	`
+	const query2 = `
+	INSERT INTO wallet_account_tr (token, bank_account_number, user_id, date, amount, type, state)
+	VALUES ('6527924beb4743c59bab8dxxxxxxxxxx', $1, $2, '2024-06-01', $3, 'Deposit', 'Pending');
+	`
 	if (!(userId && amount)) res.send("Please enter userId and amount ...");
-	fastify.pg
-		.query("", [userId, amount])
-		.then((result) => {
-			return res.send(result.rows[0]);
-		})
-		.catch((err) => {
-			res.code(500).send(err);
-		});
+	try {
+		let result = await fastify.pg.query(query, [userId])
+		const accountNumber = result.rows[0].account_number
+		result = await fastify.pg.query(query2, [accountNumber, userId, amount])
+		res.send({ "url": "https://sep.shaparak.ir/OnlinePG", "token": "6527924beb4743c59bab8dxxxxxxxxxx" });
+	} catch (error) {
+		console.error("Database query error: ", err);
+		res.status(500).send("Internal Server Error");
+	}
+
 });
+
+// Example:
+// localhost:3000/verify
+//	{
+//		"userId": 1,
+//		"token":"6527924beb4743c59bab8dxxxxxxxxxx",
+// 		"amount":"1000"
+//	}
 fastify.post("/verify", async (req, res) => {
+	const userId = req.body?.userId;
 	const token = req.body?.token;
+	const amount = req.body?.amount;
 	if (!token) res.send("Please enter token ...");
-	fastify.pg
-		.query("", [token])
-		.then((result) => {
-			return res.send(result.rows[0]);
-		})
-		.catch((err) => {
-			res.code(500).send(err);
-		});
+	const query = `
+	UPDATE wallet_account_tr
+	SET state = 'Completed'
+	WHERE token = $1 and user_id = $2;
+	`
+
+	const query2 = `
+	SELECT credit
+	from wallet
+	where user_id = $1
+	`
+
+	const query3 = `
+	UPDATE wallet
+	SET credit = $2
+	WHERE user_id = $1;
+	`
+	try {
+		let result = await fastify.pg.query(query, [token, userId])
+		let credit_old = await fastify.pg.query(query2, [userId])
+		result = await fastify.pg.query(query3, [userId, credit_old.rows[0].credit + amount])
+		res.send({ "status": "verified" }); credit
+	} catch (error) {
+		console.error("Database query error: ", err);
+		res.status(500).send("Internal Server Error");
+	}
 });
+
+
+// Example:
+// http://localhost:3000/history?userId=1
+// http://localhost:3000/history?userId=1&page=1&limit=1
 fastify.get("/history", async (req, res) => {
 	const userId = req.query?.userId;
 	if (!userId) return res.send("Please Enter userId ...");
 
 	const page = req.query?.page;
 	const limit = req.query?.limit;
+	const offset = (page - 1) * limit;
+
+	const query_offset = `
+	SELECT date, amount, type, state
+	FROM wallet_account_tr
+	WHERE user_id = $1
+	AND state = 'Completed'
+	ORDER BY date
+	LIMIT $2 OFFSET $3
+    `;
+
+	const query = `
+	SELECT date, amount, type, state
+	FROM wallet_account_tr
+	WHERE user_id = $1
+	AND state = 'Completed'
+	order by date
+    `;
 
 	if (page && limit) {
-		fastify.pg
-			.query("", [userId, page, limit])
-			.then((result) => {
-				return res.send(result.rows[0].transations);
-			})
-			.catch((err) => {
-				res.code(500).send(err);
-			});
+		try {
+			const result = await fastify.pg.query(query_offset, [userId, limit, offset]);
+			res.send(result.rows);
+		} catch (err) {
+			console.error("Database query error: ", err);
+			res.status(500).send("Internal Server Error");
+		}
+
 	} else {
-		fastify.pg
-			.query("", [userId])
-			.then((result) => {
-				return res.send(result.rows[0].transations);
-			})
-			.catch((err) => {
-				res.code(500).send(err);
-			});
+		try {
+			const result = await fastify.pg.query(query, [userId]);
+			res.send(result.rows);
+		} catch (err) {
+			console.error("Database query error: ", err);
+			res.status(500).send("Internal Server Error");
+		}
 	}
 });
+
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
 	if (err) throw err;
